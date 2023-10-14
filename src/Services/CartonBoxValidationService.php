b4cfca932d265bfcc6947364f78ebc49aa13a8c4
xBigDaddyx <yussupcazzanova97@gmail.com>
2023-10-14 21:35:00 +0700

    updated service

--- src/Services/CartonBoxValidationService.php
+++ src/Services/CartonBoxValidationService.php
@@ -11,14 +11,17 @@ class CartonBoxValidationService implements CartonBoxValidationInterface
     public function validateSolid(Model $cartonBox, string $current_polybag)
     {
         if ($cartonBox->is_completed !== true) {
-            if ($current_polybag !== $cartonBox->polybags->first()->polybag_code) {
-                return 'Polybag tidak sesuai';
+            if (!empty($cartonBox->polybags->first()->polybag_code)) {
+                if ($current_polybag !== $cartonBox->polybags->first()->polybag_code) {
+                    return 'Polybag tidak sesuai';
+                }
             }
+
             $value = new Polybag(['polybag_code' => $current_polybag]);
 
             $cartonBox->polybags()->save($value);
 
-            return 'Polybag '.$current_polybag.' berhasil ditambahkan, lanjutkan'; // Validasi solid berhasil
+            return 'Polybag ' . $current_polybag . ' berhasil ditambahkan, lanjutkan'; // Validasi solid berhasil
         }
 
         return 'carton box completed.';
