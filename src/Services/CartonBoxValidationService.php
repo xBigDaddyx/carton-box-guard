2737f93579a6629b6d2350fd6ff7e408413d99e3
xBigDaddyx <yussupcazzanova97@gmail.com>
2023-10-14 22:38:54 +0700

    added id generator

--- src/Services/CartonBoxValidationService.php
+++ src/Services/CartonBoxValidationService.php
@@ -5,23 +5,26 @@ namespace Teresa\CartonBoxGuard\Services;
 use Illuminate\Database\Eloquent\Model;
 use Teresa\CartonBoxGuard\Interfaces\CartonBoxValidationInterface;
 use Teresa\CartonBoxGuard\Models\Polybag;
+use Haruncpi\LaravelIdGenerator\IdGenerator;
+use Illuminate\Support\Facades\Auth;
 
 class CartonBoxValidationService implements CartonBoxValidationInterface
 {
     public function validateSolid(Model $cartonBox, string $current_polybag)
     {
         if ($cartonBox->is_completed !== true) {
-            if (! empty($cartonBox->polybags->first()->polybag_code)) {
+            if (!empty($cartonBox->polybags->first()->polybag_code)) {
                 if ($current_polybag !== $cartonBox->polybags->first()->polybag_code) {
                     return 'Polybag tidak sesuai';
                 }
             }
-
-            $value = new Polybag(['polybag_code' => $current_polybag]);
+            $id = IdGenerator::generate(['table' => 'polybags', 'length' => 15, 'prefix' => Auth::user()->company->id . '.PB.']);
+            //output: INV-000001
+            $value = new Polybag(['polybag_code' => $current_polybag, 'id' => $id]);
 
             $cartonBox->polybags()->save($value);
 
-            return 'Polybag '.$current_polybag.' berhasil ditambahkan, lanjutkan'; // Validasi solid berhasil
+            return 'Polybag ' . $current_polybag . ' berhasil ditambahkan, lanjutkan'; // Validasi solid berhasil
         }
 
         return 'carton box completed.';
