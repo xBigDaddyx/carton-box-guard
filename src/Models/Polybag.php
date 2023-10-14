e045a4ff729ebdf92861232a0a7f7709cbcfbad0
xBigDaddyx <xBigDaddyx@users.noreply.github.com>
2023-10-14 14:59:29 +0000

    Fix styling

--- src/Models/Polybag.php
+++ src/Models/Polybag.php
@@ -24,11 +24,11 @@ class Polybag extends Model
 
     public function __construct(array $attributes = [])
     {
-        if (!isset($this->connection)) {
+        if (! isset($this->connection)) {
             $this->setConnection(config('carton-box-guard.database_connection'));
         }
 
-        if (!isset($this->table)) {
+        if (! isset($this->table)) {
             $this->setTable(config('carton-box-guard.polybag.table_name'));
         }
 
